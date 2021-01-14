21fee2e239835f3f7cf1867b9561f797cbd99ce1
PythonEESD2021 <ctran@eesd.org>
2021-01-14 23:47:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+
+# print("Hello World")
+
+print("Hello my name is Lalyn")
+
+# print your name, your grade, your favorite animal, and favorite superhero
+
+print("I am in 16th grade.")
+print("My favorite animal is an elephant.")
+print("My favorite superhero is Iron Man!!!")
